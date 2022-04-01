c807f5cfe8a7de59e5e9087232c63e2d6593401c
glen15 <glen910115@gmail.com>
2022-04-01 18:42:27 +0900

    version 3.0

--- routes/example/index.js
+++ routes/example/index.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return 'project-02-gitaction : version 2.0'
+    return 'project-02-gitaction : version 3.0'
   })
 }
